d92043585de64506a354396be3ec1377ec5e40c3
Fausto R <fausto91@gmail.com>
2024-02-18 16:18:30 +0100

    Incremented min platforms version

--- Package.swift
+++ Package.swift
@@ -7,10 +7,10 @@ let package = Package(
     name: "PrivacyConsent",
     defaultLocalization: "en",
     platforms: [
-        .iOS(.v13),
-        .macOS(.v10_15),
-        .tvOS(.v13),
-        .watchOS(.v6)
+        .iOS(.v17),
+        .macOS(.v14),
+        .tvOS(.v17),
+        .watchOS(.v10)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
